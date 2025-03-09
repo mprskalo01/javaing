595ddff67ad1cf058f5b8cd5c38f134dd7f5576f
mprskalo01 <119109786+mprskalo01@users.noreply.github.com>
2025-03-09 20:23:37 +0100

    practicing java

---
+++ MyFirstProject/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I like lasagna."); // println or \n
+        System.out.println("It's really tasty!");
+        System.out.println("Buy me a lasagna.");
+
+    }
+}
